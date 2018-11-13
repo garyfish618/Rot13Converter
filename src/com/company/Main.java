ec2913dd3a04aa40d3f2e797ab898c5fd9977be2
Gary <fishellgary@gmail.com>
2018-11-12 21:13:14 -0800

    - Implemented ability to copy output text (Encrypted or Decrypted) to clipboard - Fixed minor bug with characters in middle of alphabet being decrypted (Ex. "N" or "n")

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,22 @@
 package com.company;
 
 import javax.swing.*;
-import java.lang.*;
+import java.awt.*;
+import java.awt.datatransfer.*;
+
 
 public class Main {
 
 
     public static void main(String[] args) {
 
+
+        String output = null;
+        String resultText;
         boolean encrypt;
+        StringBuilder result = new StringBuilder();
+
+        //Prompts user to state whether encrypting or decrypting
 
         Object[] options = {"Encrypt", "Decrypt"};
 
@@ -21,26 +29,31 @@ public class Main {
         if(a == 0)
         {
             encrypt = true;
+            resultText = "ROT 13 Line (Copied to clipboard): ";
         }
 
         else if (a == 1)
         {
             encrypt = false;
-            System.out.println("x");
+            resultText = "Decrypted ROT 13 Line (Copied to clipboard): ";
         }
 
         else
         {
             encrypt = false;
+            resultText = null;
             System.exit(0);
         }
 
         if(encrypt) {
+
+            //Asks user for input text
+
             String input = JOptionPane.showInputDialog("Enter text to be encrypted using ROT13");
 
-            StringBuilder result = new StringBuilder();
-
             for (int i = 0; i < input.length(); i++) {
+
+                //Checks if character is alphabetic, if not, does not do shift
 
                 char inputChar = input.charAt(i);
 
@@ -48,6 +61,8 @@ public class Main {
                     boolean upper;
                     char rotChar = ' ';
 
+
+                    //If it is upper case, different ascii values must be assigned
                     if (Character.isUpperCase(inputChar)) {
                         upper = true;
                     } else {
@@ -55,28 +70,26 @@ public class Main {
                     }
 
 
-                    int asciiChar = toAscii(input.charAt(i));
-                    System.out.println(asciiChar);
+                    int asciiChar = toAscii(inputChar);
+
 
                     for (int h = 0; h < 13; h++) {
 
-
                         asciiChar++;
                         rotChar = (char) (asciiChar);
-
+                        //If a one character shift goes past alphabetic ascii characters, then it is reset to front of alphabet to shift from there
                         if (!(Character.isAlphabetic(rotChar))) {
                             if (upper) {
                                 asciiChar = 65;
                             } else {
-                                System.out.println("not");
                                 asciiChar = 97;
 
                             }
+                            rotChar = (char) (asciiChar);
                         }
-
-                        System.out.println(asciiChar);
-
                     }
+
+
 
                     result.append(rotChar);
                 } else {
@@ -87,27 +100,25 @@ public class Main {
             }
 
 
-            JFrame finalResult = new JFrame();
 
-            JOptionPane.showMessageDialog(finalResult, "ROT13 Line: " + result.toString());
 
-            System.out.println("Exit");
-            System.exit(0);
+
 
 
         }
 
         else // Selected Decrypt
         {
-
+            //Asks user to input text to decrypt
             String input = JOptionPane.showInputDialog("Enter text to be decrypted using ROT13");
-
-            StringBuilder result = new StringBuilder();
 
             for(int i = 0; i < input.length(); i++)
             {
+
                 Character inChar = input.charAt(i);
                 int intChar = toAscii(inChar);
+
+                //Checks if character is alphabetic, if not, does not do shift
 
                 if(Character.isAlphabetic(inChar))
                 {
@@ -127,12 +138,16 @@ public class Main {
 
                     for(int h = 0; h < 13; h++)
                     {
+                        //Shifts back a character 13 times to get original character
+
                         intChar--;
 
                         Character decChar = (char) intChar;
 
                         if(!(Character.isAlphabetic(decChar)))
                         {
+                            //If shift goes out of bounds, reverts to end of alphabet and continues from there
+
                             if(upper)
                             {
                                 decChar = 'Z';
@@ -157,13 +172,21 @@ public class Main {
             }
 
 
-            JFrame finalResult = new JFrame();
-
-            JOptionPane.showMessageDialog(finalResult, "Decrypted ROT13 Line: " + result.toString());
-
-            System.exit(0);
 
         }
+
+
+
+        // Copies output to clipboard
+        output = result.toString();
+        StringSelection selection = new StringSelection(output);
+        Clipboard clip = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clip.setContents(selection, selection);
+
+
+        //Shows graphical display of resulting string
+        JFrame finalResult = new JFrame();
+        JOptionPane.showMessageDialog(finalResult, resultText + output);
 
 
     }
@@ -172,6 +195,7 @@ public class Main {
 
     public static int toAscii(char charToUse)
     {
+        //Converts a character to its ascii value
 
             int asciiChar = (int) charToUse;
             return asciiChar;
